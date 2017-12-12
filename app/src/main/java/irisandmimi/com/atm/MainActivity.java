56a536daa271b4f7e287abd5c9dd42c2d6c57628
IrisAndMimi <my1ris227@outlook.com>
2017-12-12 12:26:40 +0800

    不會記憶輸入過的帳號

--- app/src/main/java/irisandmimi/com/atm/MainActivity.java
+++ app/src/main/java/irisandmimi/com/atm/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,8 +67,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, UserInfoActivity.class);
+                startActivityForResult(intent,REQUEST_USERINFO);
             }
         });
     }
