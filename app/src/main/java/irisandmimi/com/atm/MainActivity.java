5d4ffe6ae61a9d4c9cc21c36b8e5120dc681e1e6
IrisAndMimi <my1ris227@outlook.com>
2017-12-19 11:43:17 +0800

    我應該要搞懂的內容,但是家裡的電腦依然沒灌好

--- app/src/main/java/irisandmimi/com/atm/MainActivity.java
+++ app/src/main/java/irisandmimi/com/atm/MainActivity.java
@@ -3,7 +3,6 @@ package irisandmimi.com.atm;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,8 +10,6 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
-import static irisandmimi.com.atm.R.id.userid;
 
 public class MainActivity extends AppCompatActivity {
     boolean logon = false;
@@ -54,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        startActivity(new Intent(this, Ch8Activity.class));
         if (!logon){
             Intent intent = new Intent(this, LoginActivity.class);
 //            startActivity(intent);
--- app/src/main/java/irisandmimi/com/atm/UserInfoActivity.java
+++ app/src/main/java/irisandmimi/com/atm/UserInfoActivity.java
@@ -1,19 +1,37 @@
 package irisandmimi.com.atm;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class UserInfoActivity extends AppCompatActivity {
     EditText edUsrnam;
     EditText edUsrph;
+    private static final String TAG = UserInfoActivity.class.getSimpleName();
+    private Spinner ages;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+       ages = (Spinner)findViewById(R.id.ages);
+        ArrayList<String>data = new ArrayList<>();
+//        for (int i = 15; i<=40; i++){
+//            data.add(i+"");
+//        }
+//        String[]data = getResources().getStringArray(R.array.ages);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
+                R.array.ages, android.R.layout.simple_list_item_1);
+        ages.setAdapter(adapter);
     }
     public void ok(View view){
+        Log.d(TAG, "ok:"+ ages.getSelectedItem().toString());
         edUsrnam = (EditText)findViewById(R.id.user_name);
         edUsrph = (EditText)findViewById(R.id.user_phone);
         String nicknam = edUsrnam.getText().toString();
